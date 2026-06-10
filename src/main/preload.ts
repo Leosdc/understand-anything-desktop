9d93e9b60ca6b90ababc4f6c236fb9d2046fc475
Leosdc <leonardo.dc.work@gmail.com>
2026-06-10 02:14:19 -0300

    feat: add getIgnoreFile and saveIgnoreFile IPC methods to preload

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -11,6 +11,8 @@ contextBridge.exposeInMainWorld("api", {
   startAnalysis: (payload: { projectPath: string; options: any }) => ipcRenderer.invoke("start-analysis", payload),
   getServerInfo: () => ipcRenderer.invoke("get-server-info"),
   openExternal: (url: string) => ipcRenderer.send("open-external", url),
+  getIgnoreFile: (projectPath: string) => ipcRenderer.invoke("get-ignore-file", projectPath),
+  saveIgnoreFile: (projectPath: string, content: string) => ipcRenderer.invoke("save-ignore-file", projectPath, content),
   onProgressUpdate: (callback: (progress: any) => void) => {
     const subscription = (_: any, data: any) => callback(data);
     ipcRenderer.on("analysis-progress-update", subscription);
